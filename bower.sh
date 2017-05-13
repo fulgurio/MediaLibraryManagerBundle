07f634b553fe7ff86d9a127aefec908c53f13e6a
Vincent G <v.guerard@fulgurio.net>
2017-05-13 17:54:41 +0200

    Error on bower shell script help message

--- bower.sh
+++ bower.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	echo 'Please type a command, like "./docker.sh install jquery --save"'
+	echo 'Please type a command, like "./bower.sh install jquery --save"'
 	exit;
 fi
 
